b38d5974afd8bd05254ba393d2dcee2a35c042ec
Krophil <pierre.brunet@krophil.fr>
2016-09-28 17:17:39 +0200

    buggy stuff

--- app/Controller/NewsController.php
+++ app/Controller/NewsController.php
@@ -25,13 +25,56 @@ class NewsController extends AppController {
 	}
 	
 	
-	public function add() {}
+	public function add() {
+		if ($this->request->is('news')) {
+            $this->News->create();
+            if ($this->News->save($this->request->data)) {
+                $this->Flash->success('L\'actualité a bien été sauvegardée');
+                return $this->redirect(array('action' => 'manage'));
+            }
+            
+            $this->Flash->error(
+            		'L\'actualité n\'a pas pu être sauvegardée. Réessayez ou contactez l\'administrateur du site'
+			);
+        }
+	}
 	
 	
-	public function edit() {}
+	public function edit($id = null) {
+		$this->News->id = $id;
+        if (!$this->News->exists()) {
+            throw new NotFoundException('Actualité invalide');
+        }
+        
+        if ($this->request->is('news') || $this->request->is('put')) {
+            if ($this->News->save($this->request->data)) {
+                $this->Flash->success('L\'actualité a bien été sauvegardée');
+                return $this->redirect(array('action' => 'manage'));
+            }
+            $this->Flash->error(
+                	'L\'actualité n\'a pas pu être sauvegardée. Réessayez ou contactez l\'administrateur du site'
+            );
+        } else {
+            $this->request->data = $this->News->findById($id);
+        }
+	}
 	
 	
-	public function delete() {}
+	public function delete($id = null) {
+		$this->request->allowMethod('news');
+
+        $this->News->id = $id;
+        if (!$this->News->exists()) {
+            throw new NotFoundException('Actualité invalide');
+        }
+        
+        if ($this->News->delete()) {
+            $this->Flash->success('Actualité supprimée avec succès');
+            return $this->redirect(array('action' => 'manage'));
+        }
+        $this->Flash->error('L\'actualité n\'a pas pu être supprimée');
+        return $this->redirect(array('action' => 'manage'));
+	}
 }
 
 ?>
